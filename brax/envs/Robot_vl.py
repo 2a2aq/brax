d1efb06d8244bb246f24ae3e5d89fa92e880605a
2a2aq <teddy2a2aq@gmail.com>
2023-01-23 20:55:31 +0900

    changed everything

--- brax/envs/Robot_vl.py
+++ brax/envs/Robot_vl.py
@@ -26,12 +26,12 @@ class Robot(env.Env):
                ctrl_cost_weight=0.1,
                healthy_reward=5.0,
                terminate_when_unhealthy=True,
-               healthy_z_range=(0.8, 2.1),
+               healthy_z_range=(0.1, 2.1),
                reset_noise_scale=1e-2,
                exclude_current_positions_from_observation=True,
                legacy_spring=False,
                **kwargs):
-    config = _SYSTEM_CONFIG_SPRING 
+    config = _SYSTEM_CONFIG_SPRING if legacy_spring else _SYSTEM_CONFIG 
     super().__init__(config=config, **kwargs)
 
     self._forward_reward_weight = forward_reward_weight
@@ -164,7 +164,882 @@ class Robot(env.Env):
   def _noise(self, rng):
     low, hi = -self._reset_noise_scale, self._reset_noise_scale
     return jp.random_uniform(rng, (self.sys.num_joint_dof,), low, hi)
-
+_SYSTEM_CONFIG = """
+  bodies {
+  name: "pelvis"
+  colliders {
+    position {
+      z: 0.07000000029802322
+    }
+    sphere {
+      radius: 0.09000000357627869
+    }
+  }
+  colliders {
+    position {
+      z: 0.20499999821186066
+    }
+    sphere {
+      radius: 0.07000000029802322
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 9.995593070983887
+}
+bodies {
+  name: "torso"
+  colliders {
+    position {
+      z: 0.11999999731779099
+    }
+    sphere {
+      radius: 0.10999999940395355
+    }
+  }
+  colliders {
+    position {
+      x: -0.011423749849200249
+      y: -0.08697725087404251
+      z: 0.2332068532705307
+    }
+    rotation {
+      x: 83.88849639892578
+      y: -7.44045352935791
+      z: 6.68812370300293
+    }
+    capsule {
+      radius: 0.04500000178813934
+      length: 0.17357417941093445
+    }
+  }
+  colliders {
+    position {
+      x: -0.011423749849200249
+      y: 0.08697725087404251
+      z: 0.2332068532705307
+    }
+    rotation {
+      x: -83.88849639892578
+      y: -7.44045352935791
+      z: -6.68812370300293
+    }
+    capsule {
+      radius: 0.04500000178813934
+      length: 0.17357417941093445
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 12.01148796081543
+}
+bodies {
+  name: "head"
+  colliders {
+    position {
+      z: 0.17499999701976776
+    }
+    sphere {
+      radius: 0.0949999988079071
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 3.8822643756866455
+}
+bodies {
+  name: "right_upper_arm"
+  colliders {
+    position {
+      z: -0.14000000059604645
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.04500000178813934
+      length: 0.27000001072883606
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 1.4993313550949097
+}
+bodies {
+  name: "right_lower_arm"
+  colliders {
+    position {
+      z: -0.11999999731779099
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.03999999910593033
+      length: 0.2150000035762787
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 0.9996799230575562
+}
+bodies {
+  name: "right_hand"
+  colliders {
+    position {
+    }
+    sphere {
+      radius: 0.03999999910593033
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 0.49997401237487793
+}
+bodies {
+  name: "left_upper_arm"
+  colliders {
+    position {
+      z: -0.14000000059604645
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.04500000178813934
+      length: 0.27000001072883606
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 1.4993313550949097
+}
+bodies {
+  name: "left_lower_arm"
+  colliders {
+    position {
+      z: -0.11999999731779099
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.03999999910593033
+      length: 0.2150000035762787
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 0.9996799230575562
+}
+bodies {
+  name: "left_hand"
+  colliders {
+    position {
+    }
+    sphere {
+      radius: 0.03999999910593033
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 0.49997401237487793
+}
+bodies {
+  name: "right_thigh"
+  colliders {
+    position {
+      z: -0.20999999344348907
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.054999999701976776
+      length: 0.4099999964237213
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 4.502291679382324
+}
+bodies {
+  name: "right_shin"
+  colliders {
+    position {
+      z: -0.20000000298023224
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.05000000074505806
+      length: 0.4099999964237213
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 2.9997496604919434
+}
+bodies {
+  name: "right_foot"
+  colliders {
+    position {
+      x: 0.04500000178813934
+      z: -0.02250000089406967
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    box {
+      halfsize {
+        x: 0.0885000005364418
+        y: 0.04500000178813934
+        z: 0.027499999850988388
+      }
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 0.999687135219574
+}
+bodies {
+  name: "left_thigh"
+  colliders {
+    position {
+      z: -0.20999999344348907
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.054999999701976776
+      length: 0.4099999964237213
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 4.502291679382324
+}
+bodies {
+  name: "left_shin"
+  colliders {
+    position {
+      z: -0.20000000298023224
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    capsule {
+      radius: 0.05000000074505806
+      length: 0.4099999964237213
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 2.9997496604919434
+}
+bodies {
+  name: "left_foot"
+  colliders {
+    position {
+      x: 0.04500000178813934
+      z: -0.02250000089406967
+    }
+    rotation {
+      x: -0.0
+      z: -0.0
+    }
+    box {
+      halfsize {
+        x: 0.0885000005364418
+        y: 0.04500000178813934
+        z: 0.027499999850988388
+      }
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 0.999687135219574
+}
+bodies {
+  name: "floor"
+  colliders {
+    plane {
+    }
+  }
+  inertia {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+  mass: 1.0
+  frozen {
+    all: true
+  }
+}
+  joints {
+  name: "abdomen_xyz"
+  parent: "pelvis"
+  child: "torso"
+  parent_offset {
+    z: 0.23615099489688873
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -60.0
+    max: 60.0
+  }
+  angle_limit {
+    min: -60.0
+    max: 90.0
+  }
+  angle_limit {
+    min: -50.0
+    max: 50.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "neck_xyz"
+  parent: "torso"
+  child: "head"
+  parent_offset {
+    z: 0.22389400005340576
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -50.0
+    max: 50.0
+  }
+  angle_limit {
+    min: -40.0
+    max: 60.0
+  }
+  angle_limit {
+    min: -45.0
+    max: 45.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "right_shoulder_xyz"
+  parent: "torso"
+  child: "right_upper_arm"
+  parent_offset {
+    x: -0.024049999192357063
+    y: -0.18310999870300293
+    z: 0.2434999942779541
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -180.0
+    max: 45.0
+  }
+  angle_limit {
+    min: -180.0
+    max: 60.0
+  }
+  angle_limit {
+    min: -90.0
+    max: 90.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "right_elbow"
+  parent: "right_upper_arm"
+  child: "right_lower_arm"
+  parent_offset {
+    z: -0.2747879922389984
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: 90.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -160.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "$right_lower_arm.right_hand"
+  parent: "right_lower_arm"
+  child: "right_hand"
+  parent_offset {
+    z: -0.2589470148086548
+  }
+  rotation {
+    x: -0.0
+    y: -90.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -5.0
+    max: 5.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "left_shoulder_xyz"
+  parent: "torso"
+  child: "left_upper_arm"
+  parent_offset {
+    x: -0.024049999192357063
+    y: 0.18310999870300293
+    z: 0.2434999942779541
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -45.0
+    max: 180.0
+  }
+  angle_limit {
+    min: -180.0
+    max: 60.0
+  }
+  angle_limit {
+    min: -90.0
+    max: 90.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "left_elbow"
+  parent: "left_upper_arm"
+  child: "left_lower_arm"
+  parent_offset {
+    z: -0.2747879922389984
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: 90.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -160.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "$left_lower_arm.left_hand"
+  parent: "left_lower_arm"
+  child: "left_hand"
+  parent_offset {
+    z: -0.2589470148086548
+  }
+  rotation {
+    x: -0.0
+    y: -90.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -5.0
+    max: 5.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "right_hip_xyz"
+  parent: "pelvis"
+  child: "right_thigh"
+  parent_offset {
+    y: -0.08488699793815613
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -60.0
+    max: 15.0
+  }
+  angle_limit {
+    min: -140.0
+    max: 60.0
+  }
+  angle_limit {
+    min: -60.0
+    max: 35.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "right_knee"
+  parent: "right_thigh"
+  child: "right_shin"
+  parent_offset {
+    z: -0.42154601216316223
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: 90.0
+  }
+  angular_damping: 30
+  angle_limit {
+    max: 160.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "right_ankle_xyz"
+  parent: "right_shin"
+  child: "right_foot"
+  parent_offset {
+    z: -0.4098699986934662
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -30.0
+    max: 30.0
+  }
+  angle_limit {
+    min: -55.0
+    max: 55.0
+  }
+  angle_limit {
+    min: -40.0
+    max: 40.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "left_hip_xyz"
+  parent: "pelvis"
+  child: "left_thigh"
+  parent_offset {
+    y: 0.08488699793815613
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -15.0
+    max: 60.0
+  }
+  angle_limit {
+    min: -140.0
+    max: 60.0
+  }
+  angle_limit {
+    min: -35.0
+    max: 60.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+  
+joints {
+  name: "left_knee"
+  parent: "left_thigh"
+  child: "left_shin"
+  parent_offset {
+    z: -0.42154601216316223
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: 90.0
+  }
+  angular_damping: 30
+  angle_limit {
+    max: 160.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+joints {
+  name: "left_ankle_xyz"
+  parent: "left_shin"
+  child: "left_foot"
+  parent_offset {
+    z: -0.4098699986934662
+  }
+  child_offset {
+  }
+  rotation {
+    x: -0.0
+    z: -0.0
+  }
+  angular_damping: 30
+  angle_limit {
+    min: -30.0
+    max: 30.0
+  }
+  angle_limit {
+    min: -55.0
+    max: 55.0
+  }
+  angle_limit {
+    min: -40.0
+    max: 40.0
+  }
+  reference_rotation {
+    x: -0.0
+    z: -0.0
+  }
+}
+actuators {
+  name: "neck_xyz"
+  joint: "neck_xyz"
+  strength: 20.0
+  torque {
+  }
+}
+actuators {
+  name: "abdomen_xyz"
+  joint: "abdomen_xyz"
+  strength: 125.0
+  torque {
+  }
+}
+actuators {
+  name: "right_shoulder_xyz"
+  joint: "right_shoulder_xyz"
+  strength: 70.0
+  torque {
+  }
+}
+actuators {
+  name: "right_elbow"
+  joint: "right_elbow"
+  strength: 60.0
+  torque {
+  }
+}
+actuators{
+  name: "$right_lower_arm.right_hand"
+  joint: "$right_lower_arm.right_hand"
+  strength: 300.0
+  torque{
+  }
+}
+actuators {
+  name: "left_shoulder_xyz"
+  joint: "left_shoulder_xyz"
+  strength: 70.0
+  torque {
+  }
+}
+actuators {
+  name: "left_elbow"
+  joint: "left_elbow"
+  strength: 60.0
+  torque {
+  }
+}
+actuators{
+  name: "$left_lower_arm.left_hand"
+  joint: "$left_lower_arm.left_hand"
+  strength: 300.0
+  torque{
+  }
+}
+actuators {
+  name: "right_hip_xyz"
+  joint: "right_hip_xyz"
+  strength: 125.0
+  torque {
+  }
+}
+actuators {
+  name: "right_knee"
+  joint: "right_knee"
+  strength: 100.0
+  torque {
+  }
+}
+actuators {
+  name: "right_ankle_xyz"
+  joint: "right_ankle_xyz"
+  strength: 50.0
+  torque {
+  }
+}
+actuators {
+  name: "left_hip_xyz"
+  joint: "left_hip_xyz"
+  strength: 125.0
+  torque {
+  }
+}
+actuators {
+  name: "left_knee"
+  joint: "left_knee"
+  strength: 100.0
+  angle {
+  }
+}
+actuators {
+  name: "left_ankle_xyz"
+  joint: "left_ankle_xyz"
+  strength: 50.0
+  angle {
+  }
+}
+  collide_include {
+    first: "floor"
+    second: "left_foot"
+  }
+  collide_include {
+    first: "floor"
+    second: "right_foot"
+  }
+  defaults {
+    angles {
+      name: "left_knee"
+      angle { x: -25. y: 0 z: 0 }
+    }
+    angles {
+      name: "right_knee"
+      angle { x: -25. y: 0 z: 0 }
+    }
+  }
+  friction: 1.0
+  gravity {
+    z: -9.81
+  }
+  angular_damping: -0.05
+  dt: 0.015
+  substeps: 8
+  dynamics_mode: "pbd"
+  """
 _SYSTEM_CONFIG_SPRING = """
 bodies {
   name: "pelvis"
@@ -1413,51 +2288,35 @@ actuators {
   name: "left_knee"
   joint: "left_knee"
   strength: 100.0
-  torque {
+  angle {
   }
 }
 actuators {
   name: "left_ankle_x"
   joint: "left_ankle_x"
   strength: 50.0
-  torque {
+  angle {
   }
 }
 actuators {
   name: "left_ankle_y"
   joint: "left_ankle_y"
   strength: 50.0
-  torque {
+  angle {
   }
 }
 actuators {
   name: "left_ankle_z"
   joint: "left_ankle_z"
   strength: 50.0
-  torque {
+  angle {
   }
-}
-collide_include {
-    first: "floor"
-    second: "left_shin"
-}
-collide_include {
-    first: "floor"
-    second: "right_shin"
-}
-collide_include {
-    first: "floor"
-    second: "left_foot"
-}
-collide_include {
-    first: "floor"
-    second: "right_foot"
 }
 defaults{
   angles{
     name: "left_knee"
     angle {
-      x:40
+      x:60
     }
   }
   angles{
@@ -1477,10 +2336,10 @@ defaults{
     }
   }
 }
-friction: 1.0
+friction: 0.0
 gravity { z: -9.8 }
 angular_damping: -0.05
 dt: 0.05
-substeps: 30
+substeps: 10
 dynamics_mode: "pbd"
 """
